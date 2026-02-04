c866f0f59f2e8c7466c8762561a915211f7fcea1
CalculatedCode <fostdavi@oregonstate.edu>
2026-02-03 17:28:31 -0800

    Removed duplicate file 'read_multiple' and updated frontend_read using simple_functions.read

--- db/functions/frontend_read.py
+++ db/functions/frontend_read.py
@@ -1,9 +1,7 @@
-import read_multiple
-from connect import get_db
+from db.functions.simple_functions import read
+from db.functions.connect import get_db
 
 
 def get_locations():
-    location_data = read_multiple.view_locations(columns=['location_id', 'name'])
+    location_data = read.view_locations(columns=['location_id', 'name'])
     return location_data
-
-print(get_locations())
