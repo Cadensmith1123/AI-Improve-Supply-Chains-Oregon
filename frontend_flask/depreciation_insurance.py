a6f317c11172c1c965a80841ae4a5556d3082d42
CalculatedCode <fostdavi@oregonstate.edu>
2026-03-03 14:45:44 -0800

    included the depreciation and insurance from Luke into logic.py

--- frontend_flask/depreciation_insurance.py
+++ frontend_flask/depreciation_insurance.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+USEFUL_LIFE_YEARS = 8
+
 def _to_float(x) -> float:
     try:
         return float(x)
@@ -13,14 +15,16 @@ def _safe_div(numer, denom) -> float:
     return _to_float(numer) / d
 
 # Depreciation
-def depreciation_cost_per_mile(purchase_price, salvage_value, total_projected_mileage) -> float:
+def depreciation_cost_per_mile(purchase_price, salvage_value, expected_annual_mileage) -> float:
+    total_projected_mileage = expected_annual_mileage * USEFUL_LIFE_YEARS if expected_annual_mileage > 0 else 0.0
     pp = _to_float(purchase_price)
     sv = _to_float(salvage_value)
     miles = _to_float(total_projected_mileage)
     base = max(pp - sv, 0.0)
     return _safe_div(base, miles)
 
-def trip_depreciation_cost(purchase_price, salvage_value, total_projected_mileage, trip_miles) -> float:
+def trip_depreciation_cost(purchase_price, salvage_value, expected_annual_mileage, trip_miles) -> float:
+    total_projected_mileage = expected_annual_mileage * USEFUL_LIFE_YEARS if expected_annual_mileage > 0 else 0.0
     dppm = depreciation_cost_per_mile(purchase_price, salvage_value, total_projected_mileage)
     return dppm * _to_float(trip_miles)
 
