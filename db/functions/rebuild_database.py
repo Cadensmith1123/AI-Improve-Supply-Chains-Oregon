971d0d43f7c9b399ec914714852b6a54b7fb58b7
CalculatedCode <fostdavi@oregonstate.edu>
2026-03-03 13:43:53 -0800

    adjusted rebuild database to not default to local food

--- db/functions/rebuild_database.py
+++ db/functions/rebuild_database.py
@@ -28,7 +28,8 @@ def get_db_connection():
         password=os.getenv("DB_PASSWORD"),
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT"),
-        database=os.getenv("DB_NAME") # Default to local_food_db if not set
+        database=os.getenv("DB_NAME"), # Default to local_food_db if not set
+        use_pure=True
     )
 
 def execute_sql_file(conn, file_path):
